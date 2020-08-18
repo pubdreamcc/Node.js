6fff4d4fbea02ae2e175187280b41b2c5cd65f64
pubdreamcc <493310907@qq.com>
2020-08-18 16:01:54 +0800

    更新 爬取掘金 文章的接口地址

--- Node学习demo案例/Express/爬取掘金资源/app.js
+++ Node学习demo案例/Express/爬取掘金资源/app.js
@@ -1,59 +1,36 @@
-const express = require('express')
-const cheerio = require('cheerio')
-const superagent = require('superagent')
-const app = express()
-let result = []
-app.use('/public', express.static('./public'))
-app.engine('html', require('express-art-template'))
+const express = require('express');
+const cheerio = require('cheerio');
+const superagent = require('superagent');
+const app = express();
+let result = [];
+app.use('/public', express.static('./public'));
+app.engine('html', require('express-art-template'));
 let params = {
-  operationName:"",
-  query:"",
-  variables: {
-    first:20,
-    after: "",
-    order: "POPULAR"
-  },
-  extensions: {
-    query: {
-      id: "21207e9ddb1de777adeaca7a2fb38030"
-    }
-  }
+  category: "frontend",
+  limit: 30,
+  offset: 0,
+  order: "time"
 }
 function getInfo () {
-  superagent.post('https://web-api.juejin.im/query').send(params).set('X-Agent', 'Juejin/Web').end((err, res) => {
+  superagent.post('https://e.xitu.io/resources/gold')
+    .send(params)
+    .end((err, res) => {
     if (err) {
       return console.log(err)
     } 
-    const array1 = JSON.parse(res.text).data.articleFeed.items.edges
-    const num = JSON.parse(res.text).data.articleFeed.items.pageInfo.endCursor
-    result = array1.filter(item => {
-      return item.node.likeCount > 50
-    })
-    params.variables.after = num.toString()
-    superagent.post('https://web-api.juejin.im/query').send(params).set('X-Agent', 'Juejin/Web').end((err, res) => {
-      if (err) {
-        return console.log(err)
-      }
-      const array2 = JSON.parse(res.text).data.articleFeed.items.edges
-      const result2 = array2.filter(item => {
-        return item.node.likeCount > 50
-      })
-      result2.forEach(item => {
-        result.push(item)
-      })
-    })
+    result = res.body.data;
   })
 }
-getInfo()
+getInfo();
 setInterval(() => {
   getInfo()
-}, 10*1000*60)
+}, 10*1000*24);
 
 app.get('/', (req, res, next) => {
   res.render('index.html', {
     result
-  })
+  });
 })
 app.listen(3000, () => {
   console.log('running...')
-})
+});
